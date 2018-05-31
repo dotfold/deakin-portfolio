c7fd3d0171a782c179a7b7ccbc561ca8575f577c
dotfold <james@dotfold.io>
2018-05-30 23:23:37 -0700

    feat(index): remove boilerplate sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,33 @@
 import React from 'react'
-import ExperienceSection from '../components/ExperienceSection'
-import ProjectsSection from '../components/ProjectsSection'
 import About from '../components/Sidebar/About'
 import Links from '../components/Sidebar/Links'
 
 import './style.scss'
 
 const IndexPage = () => (
-    <div className="index">
-      <div className="main">
-        <h5>
-          Hi, I'm <span className="bold">Praagya Joshi</span>
-        </h5>
+  <div className='index'>
+    <div className='main'>
+      <h5>
+        Hi, I'm <span className='bold'>James McNamee</span>
+      </h5>
 
-        <h3 className="bold">
-          Full stack product engineer with 3 years' experience building products
-          for with a leading food tech company, an early stage fitness start up
-          and a leading SaaS provider.
-        </h3>
+      <h3 className='bold'>
+        Stuff about me
+      </h3>
 
-        <ExperienceSection />
-        <ProjectsSection />
+      {/* <ExperienceSection /> */}
+      {/* <ProjectsSection /> */}
+    </div>
+
+    <div className='aside'>
+      <div className='top'>
+        <About />
       </div>
-
-      <div className="aside">
-        <div className="top">
-          <About />
-        </div>
-        <div className="bottom">
-          <Links />
-        </div>
+      <div className='bottom'>
+        <Links />
       </div>
     </div>
+  </div>
 )
 
 export default IndexPage
